8509b5291045f2b4db377a98c33787d339b51033
michaelborck <michael@borck.me>
2022-05-07 18:40:03 +0800

    added contact.css to sw

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,8 @@
 let cacheName = "cc-cache";
 let filesToCache = ["/", "index.html", "about.html", "contact.html",
                     "explore.html", "profile.html", "components/header.js",
-                    "css/style.css", "css/login.css", "js/main.js",
-                    "js/firebase.js", "js/login.js"];
+                    "css/style.css", "css/login.css", "css/contact.css",
+                    "js/main.js", "js/firebase.js", "js/login.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", (e) => {
